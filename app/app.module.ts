83d62f450037821b37cac37cc08b25e6c95ebbdc
Todd Motto <todd@toddmotto.com>
2017-03-13 12:37:00 +0000

    🕶Release the seed project

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,12 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-import { DynamicFormModule } from './dynamic-form/dynamic-form.module';
-
 @NgModule({
   imports: [
-    BrowserModule,
-    DynamicFormModule
+    BrowserModule
   ],
   bootstrap: [
     AppComponent
--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,5 @@
+import 'core-js/es7/reflect';
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
